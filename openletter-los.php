d85f1ad5eb74f45281898572eff829494c6fd640
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-04-22 20:51:58 +0200

    first commit

---
+++ openletter-los.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/kollektiv-kpunkt/cpv-testimonial
+ * @since             1.0.0
+ * @package           CPV Testimonial
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CPV Testimonial
+ * Plugin URI:        https://github.com/kollektiv-kpunkt/cpv-testimonial
+ * Description:       Plugin for cantonal initiative in the état de vaud for creating testimonials of support.
+ * Version:           1.0.0
+ * Author:            Timothy@K.
+ * Author URI:        https://kpunkt.ch/
+ * GitHub Plugin URI: https://github.com/kollektiv-kpunkt/cpv-testimonial
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ * Text Domain:       cpv-testimonial
+*/
